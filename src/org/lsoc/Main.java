27aa3c5a089a4c73318052a3c17d0dcaf819b8c1
Paloma <pcerda@lsoc.org>
2025-08-27 16:18:47 +0000

    Rewrote Stories

--- src/org/lsoc/Main.java
+++ src/org/lsoc/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("I went tubing and was flung into the air and water multipule times. Yay!");
     }
 }
